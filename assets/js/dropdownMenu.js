e776b7701bea321feb8df5b1de9381f5ce01e3a7
MannB1023 <davidlacava66@gmail.com>
2023-07-04 10:34:44 -0400

    moving to pc

--- assets/js/dropdownMenu.js
+++ assets/js/dropdownMenu.js
@@ -1,5 +1,5 @@
 const toggleBtn = document.querySelector('.menu-icon');
-const toggleBtnIcon = document.querySelector('.menu-icon i');
+const toggleBtnIcon = document.querySelector('.menu-icon img');
 const dropDownMenu = document.querySelector('.dropdown-menu');
 
 toggleBtn.onclick = function(){
@@ -7,16 +7,16 @@ toggleBtn.onclick = function(){
     const isOpen = dropDownMenu.classList.contains('open');
 
     toggleBtnIcon.classList = isOpen 
-    ? 'fa-solid fa-xmark fa-lg'
-    : 'fa-solid fa-bars fa-lg';
+    ? 'close'
+    : 'bars';
 }
 
 // Close the dropdown menu when an item is clicked
 const menuItems = document.querySelectorAll('.dropdown-menu a');
 for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].addEventListener('click', function() {
-    dropDownMenu.classList.remove('open');
-    toggleBtnIcon.classList = 'fa-solid fa-bars fa-lg';
+        dropDownMenu.classList.remove('open');
+        toggleBtnIcon.classList = 'bars';
     });
 }
 
@@ -27,7 +27,7 @@ document.addEventListener('click', function(event) {
     const isClickOnToggleButton = toggleBtn.contains(targetElement);
 
     if (!isClickInsideMenu && !isClickOnToggleButton) {
-    dropDownMenu.classList.remove('open');
-    toggleBtnIcon.classList = 'fa-solid fa-bars fa-lg';
+        dropDownMenu.classList.remove('open');
+        toggleBtnIcon.classList = 'bars';
     }
 });
