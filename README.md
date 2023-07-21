8263caa8e852d1b268800f66be57a67332c2e71b
MannB1023 <davidlacava66@gmail.com>
2023-07-21 02:10:13 -0400

    added custom scroll progress bar

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ change size of text/layout for mobile
 
 https://www.youtube.com/watch?v=1wfeqDyMUx4
 
-https://codepen.io/tutsplus/pen/dMvJaO
-
 # See me here
 [Website](https://davidl1023.github.io/)
 
--- assets/js/animations.js
+++ assets/js/animations.js
@@ -7,6 +7,7 @@
         $isAnimatedThird = $('.third .is-animated'),
         $isAnimatedFourth = $('.fourth .is-animated'),
         $isAnimatedFifth = $('.fifth .is-animated')
+    var $progressBar = $('#scroll-progress');
 
     // initialize fullPage
     $('#fullpage').fullpage({
@@ -19,10 +20,9 @@
       fixedElements: '#navbar',
       scrollingSpeed: 1100,
       scrollOverflow: false,
-      navigation: true,
       loopHorizontal: false,
       slidesNavigation: true,
-
+      
       onLeave: function(index, nextIndex, direction) {
       
         /**
@@ -37,9 +37,8 @@
         */
         var i = index.index;
         var n = nextIndex.index;
-        console.log(i, n);
 
-        // first animation
+        // first slide animation
         if( i == 0 && n == 1 ) { 
             $isAnimatedSecond.addClass('blur-in-slide-up');
         }
@@ -59,8 +58,23 @@
             $isAnimatedFifth.addClass('blur-in-slide-up');
         }
 
+      },
+
+      afterLoad: function(anchorLink, index){
+        // progress bar animation
+        if(index.index == 0){
+            $progressBar.css('height', '5rem');
+        }else if(index.index == 1){
+            $progressBar.css('height', '10rem');
+        }else if(index.index == 2){
+            $progressBar.css('height', '15rem');
+        }else if(index.index == 3){
+            $progressBar.css('height', '20rem');
+        }else if(index.index == 4){
+            $progressBar.css('height', '25rem');
+        }
       }
-  
+
     });
     
   })(jQuery);
